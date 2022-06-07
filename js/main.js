e7abb64d182c2f5dc9a6c14b6a0ca9a32299a59f
Tom Wagg <tomjwagg@gmail.com>
2022-06-07 16:52:07 -0700

    add a footer

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
 window.MathJax = {
     tex: {
-        inlineMath: [['$', '$'], ['\\(', '\\)']]
+        inlineMath: [
+            ['$', '$'],
+            ['\\(', '\\)']
+        ]
     },
     inTabOrder: false,
     startup: {
-        pageReady: function() {
+        pageReady: function () {
             return MathJax.startup.defaultPageReady().then(function () {
-                document.querySelectorAll(".input-group-text .MathJax").forEach(function(el) {
+                document.querySelectorAll(".input-group-text .MathJax").forEach(function (el) {
                     el.setAttribute("tabindex", "-1")
                 })
             })
         }
     }
 };
+
+
+$('body').on('scroll', function () {
+    // will give position of scrollbar
+    var height = $(this).scrollHeight();
+    if (height > 100) {
+        // this will update the font size
+        $('.navbar-brand').addClass('scrollFontSize')
+    } else if (height === 0) {
+        // removing specific font size on scrolling back to top
+        $('.navbar-brand').removeClass('scrollFontSize')
+    }
+})
