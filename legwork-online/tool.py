f7839af19441a3c53a2548fbb3d8d5b51139349c
Tom Wagg <tomjwagg@gmail.com>
2022-06-14 17:07:09 -0700

    some linting

--- legwork-online/tool.py
+++ legwork-online/tool.py
@@ -1,56 +1,16 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, render_template, request
 )
-from werkzeug.exceptions import abort
 
 import legwork
 import numpy as np
 import astropy.units as u
 import time
 
-import os
 import matplotlib
 matplotlib.use('Agg')
 
 bp = Blueprint('tool', __name__)
-
-
-# def source_to_json(sources):
-#     single_source = source.n_sources == 1
-
-#     sources_dict = {}
-#     if single_source:
-#         sources_dict = {
-#             "m_1": sources.m_1.to(u.Msun).value[0],
-#             "m_2": sources.m_2.to(u.Msun).value[0],
-#             "f_orb": sources.f_orb.to(u.mHz).value[0],
-#             "ecc": sources.ecc[0],
-#             "dist": sources.dist.to(u.kpc).value[0],
-#             "t_merge": sources.t_merge.to(u.yr).value[0],
-#             "h_0": sources.m_1.to(u.Msun).value[0],
-#             "h_c": sources.m_1.to(u.Msun).value[0],
-#             "snr": sources.snr.value[0]
-#         }
-#     else:
-
-
-#     json = {
-#         "single_source": single_source,
-#         "sources": sources_dict,
-#         "detector": {
-#             "instrument": sources._sc_params["instrument"],
-#             "duration": sources._sc_params["t_obs"],
-#             "approximate_response_function": sources._sc_params["approximate_R"],
-#             "confusion_noise_model": sources._sc_pararms["confusion_noise"]
-#         },
-#         "settings": {
-#             "gw_lum_tol": sources._gw_lum_tol,
-#             "stat_tol": sources.stat_tol,
-#             "interpolate_g": false,
-#             "interpolate_sc": true
-#         }
-#     }
-#     return json
 
 
 @bp.route('/')
@@ -88,11 +48,13 @@ def t_merge():
     }
     return json
 
+
 @bp.route('/tool/log_total_strain', methods=["POST"])
 def log_total_strain():
     start = time.time()
     data = request.get_json()
     return total_strains(data, "log_total_strain", start)
+
 
 @bp.route('/tool/log_total_char_strain', methods=["POST"])
 def log_total_char_strain():
